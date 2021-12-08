b5a713ed562a7a5e1aa7a336746b0ca8e2223e50
mhmdho <hadiomidm@gmail.com>
2021-12-08 23:33:38 +0330

    Add CW21_14000916_Tue

---
+++ CW21_14000916_Tue/myBlog/post/serializers.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from post.models import Category, Post, Tag, Comment
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'created_at', 'author']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'owner', 'description', 'post_id']
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    posts = PostSerializer(source='category_posts', many=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'posts']
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+    tag = TagSerializer(many=True)
+    category = CategorySerializer(many=True)
+    comment = CommentSerializer(source='post_comment', many=True)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
+    post = PostSerializer()
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
